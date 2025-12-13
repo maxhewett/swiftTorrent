3ff4eacaee4f2c27968624783e0664e17d7c4026
Max Hewett <max@maxhewett.com>
2025-12-14 12:39:40 +1300

    Initial Commit

---
+++ swiftTorrent/swiftTorrentApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftTorrentApp.swift
+//  swiftTorrent
+//
+//  Created by Max Hewett on 14/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct swiftTorrentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
